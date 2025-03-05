f11b6ff080df2b8919b15b4c7b71c9623f077e4a
Daniel Ferraz <dxferraz@gmail.com>
2025-03-04 22:12:35 -0300

    Custom domain

--- src/server.js
+++ src/server.js
@@ -5,15 +5,15 @@ const personaRoutes = require("./routes/invenstoryRoutes");
 const setupSwagger = require("./swagger");
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: ["https://api.invenstory.dev"] }));
 app.use(express.json());
 
-app.use("/api", personaRoutes);
+app.use("/", personaRoutes);
 
 setupSwagger(app);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(
     `🚀 Server running! Access http://localhost:${PORT}/api-docs/ to see documentation.`
